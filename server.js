f8da6f9aba841388c20a4cce17590fc89aec5136
Duck <77748099+alperalper00@users.noreply.github.com>
2025-05-19 14:18:02 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import Mesaj from './messageContent.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const unlockDate = new Date('2025-11-08T00:00:00Z');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/message', (req, res) => {
+  if (new Date() >= unlockDate) {
+    return res.status(200).json({ message: Mesaj });
+  } else {
+    return res.status(403).json({ error: 'Mesaj henüz kilitli.' });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Sunucu http://localhost:${PORT} üzerinde çalışıyor.`)
+);
