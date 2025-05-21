aaa0c77e04d992f6ffa96185659e7d0454f346e8
Duck <77748099+alperalper00@users.noreply.github.com>
2025-05-21 18:36:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,58 @@
+// server.js
 import express from 'express';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 import Mesaj from './messageContent.js';
+
+dotenv.config(); 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const unlockDate = new Date('2025-11-08T00:00:00Z');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.post('/check-password', (req, res) => {
+  const enteredPassword = req.body.password;
+  const correctPassword = process.env.PASSWORD; 
+
+  if (!enteredPassword) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Şifre sağlanmadı.' });
+  }
+
+  if (enteredPassword === correctPassword) {
+    return res.status(200).json({ success: true });
+  } else {
+    return res
+      .status(401)
+      .json({ success: false, message: 'Şifre yanlış.' });
+  }
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+const unlockDate = new Date('2025-11-08T00:00:00Z');
 app.get('/api/message', (req, res) => {
   if (new Date() >= unlockDate) {
     return res.status(200).json({ message: Mesaj });
   } else {
-    return res.status(403).json({ error: 'Mesaj henüz kilitli.' });
+    return res
+      .status(403)
+      .json({
+        error:
+          'Rümü Sayfa Kaynağından Mesajı Görme Diye Kilitledim HİHİHİ',
+      });
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log(`Sunucu http://localhost:${PORT} üzerinde çalışıyor.`)
-);
+app.listen(PORT, () => {
+  console.log(`Sunucu http://localhost:${PORT} üzerinde çalışıyor.`);
+});
